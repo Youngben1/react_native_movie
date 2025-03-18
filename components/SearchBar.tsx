b7a6e1381ea829ce1a4c31e947acd4d34cd1713c
Youngben1 <benjaminrenny@gmail.com>
2025-03-18 16:29:02 +0100

    fixed bugs and implementing home screen section

---
+++ components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import { icons } from '@/constants/icons'
+import { View, Image, TextInput } from 'react-native'
+
+const SearchBar = () => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className="size-5" resizeMode='contain' tintColor="#ab8bff" />
+      <TextInput onPress={() => {}} placeholder='Search' onChange={() => {}} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white' />
+    </View>
+  )
+}
+
+export default SearchBar
