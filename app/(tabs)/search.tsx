092a56056898b76b09c3b020496a26c738202979
Youngben1 <benjaminrenny@gmail.com>
2025-03-21 16:38:18 +0100

    implemented appwrite to the project

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,6 +6,7 @@ import MovieCard from '@/components/MovieCard';
 import { fetchMovies } from '@/sevices/api';
 import { icons } from '@/constants/icons';
 import SearchBar from '@/components/SearchBar';
+import { updateSearchCount } from '@/sevices/appwrite';
 
 const Search = () => {
  const [searchQuery, setSearchQuery] = useState('');
@@ -22,6 +23,8 @@ const Search = () => {
     const timeoutId = setTimeout (async () => {
       if(searchQuery.trim()) {
         await loadMovies();
+
+        if(movies?.length > 0 && movies?.[0]) await updateSearchCount(searchQuery, movies[0]);
       } else {
         reset()
       }
